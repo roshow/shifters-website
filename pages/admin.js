504f00d08662aa41851181bc966e9a966249f012
roshow <roshow@gmail.com>
2020-01-11 23:40:12 -0500

    initial view page and admin page and next setup of course

---
+++ pages/admin.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const AdminPage = () => {
+
+  const [didPublish, setDidPublish] = useState(false);
+
+  const [isPublishing, setIsPublishing] = useState(false);
+
+  const [isErrorPublishing, setIsErrorPublishing] = useState(false);
+
+  const publishChapters = async () => {
+    setIsPublishing(true);
+    setDidPublish(false);
+    try {
+      await fetch('http://kind-sinoussi-a45d2a.netlify.com/.netlify/functions/create-shifters-index', {
+        method: 'POST',
+      });
+      setDidPublish(true);
+    } catch(e) {
+      setIsErrorPublishing(true);
+    }
+    setIsPublishing(false);
+  };
+
+  return (
+    <div>
+      {isPublishing && <h4>Publishing in progress...</h4>}
+      
+      {isErrorPublishing 
+        && <h4>Something went wrong. Refresh the page & try again. If it still fails, tell Rolando.</h4>}
+      
+      {didPublish && !isErrorPublishing && <h4>Last publish was successful.</h4>}
+
+      {!isPublishing && !isErrorPublishing && <button onClick={publishChapters}>Publish Shifters Chapters</button>}
+    </div>
+  );
+};
+
+export default AdminPage;
+
+
